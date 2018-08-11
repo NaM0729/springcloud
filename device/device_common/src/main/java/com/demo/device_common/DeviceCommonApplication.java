e1112a0ba925d16fdeb1f953f72de128bdb3578e
heart_mine <zynxmy@163.com>
2018-08-11 18:34:33 +0800

    【+】添加服务端注册、客户端注册、设备模块框架

---
+++ device/device_common/src/main/java/com/demo/device_common/DeviceCommonApplication.java
@@ -0,0 +1,12 @@
+package com.demo.device_common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceCommonApplication.class, args);
+	}
+}
