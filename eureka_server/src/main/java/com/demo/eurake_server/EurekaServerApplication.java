039e4fefdef6e273878736aa56265514028d449f
heart_mine <zynxmy@163.com>
2018-08-18 15:18:41 +0800

    【#】修改eureka与项目从属关系

--- eureka_server/src/main/java/com/demo/eurake_server/EurekaServerApplication.java
+++ eureka_server/src/main/java/com/demo/eurake_server/EurekaServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurakeServerApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurakeServerApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 }
