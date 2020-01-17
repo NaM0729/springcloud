6f3a2263f0be41ea6660031d1b839a3b55c3d793
heart_mine <zynxmy@163.com>
2020-01-17 10:56:51 +0800

    [+] netty 客户端增加超时判定和监听

--- netty-demo/src/main/java/com/test/nettydemo/tcp/chat/ChatClient.java
+++ netty-demo/src/main/java/com/test/nettydemo/tcp/chat/ChatClient.java
@@ -2,7 +2,9 @@ package com.test.nettydemo.tcp.chat;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -32,6 +34,7 @@ public class ChatClient {
             Bootstrap bootstrap = new Bootstrap();
             bootstrap.group(bossGroup)
                     .channel(NioSocketChannel.class)
+                    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
                     .handler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel socketChannel) throws Exception {
@@ -42,14 +45,27 @@ public class ChatClient {
                                     .addLast(new ChatClientHandle());
                         }
                     });
-            ChannelFuture future = bootstrap.connect("127.0.0.1", 8080).sync();
-            Scanner scanner = new Scanner(System.in);
-//            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+            ChannelFuture future = bootstrap.connect("127.0.0.1", 80).addListener((ChannelFutureListener) future1 -> {
+                if (!future1.isSuccess()) {
+                    System.out.println("超时");
+                } else {
+                    System.out.println("连接成功");
+                }
+            }).sync();
 
-            while (true) {
-                String next = scanner.next();
-                future.channel().writeAndFlush(next + "\n");
-            }
+//            // 控制台输入
+//            Scanner scanner = new Scanner(System.in);
+////            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+//
+//            while (true) {
+//                String next = scanner.next();
+//                future.channel().writeAndFlush(next + "\n");
+//            }
+
+            // 阻塞通道关闭 用于非控制台输入
+//            future.channel().closeFuture().sync();
+
+
         } catch (Exception e) {
 
         } finally {
--- netty-demo/src/main/java/com/test/nettydemo/tcp/chat/ChatServerHandler.java
+++ netty-demo/src/main/java/com/test/nettydemo/tcp/chat/ChatServerHandler.java
@@ -1,18 +1,18 @@
 package com.test.nettydemo.tcp.chat;
 
+import com.alibaba.fastjson.JSONObject;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.util.concurrent.GlobalEventExecutor;
-import sun.applet.resources.MsgAppletViewer;
+import org.omg.CORBA.OBJ_ADAPTER;
 
-import java.net.SocketAddress;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author zyn
@@ -24,14 +24,45 @@ public class ChatServerHandler extends SimpleChannelInboundHandler<String> {
 
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, String s) throws Exception {
-        Channel incoming = channelHandlerContext.channel();
-        for (Channel channel : channels) {
-            if (channel != incoming) {
-                channel.writeAndFlush(incoming.remoteAddress() + "说：" + s + "\n");
-            } else {
-                channel.writeAndFlush("自己说：" + s + "\n");
+        System.out.println(channelHandlerContext.channel().remoteAddress() + "开始发送数据：" + s);
+        while (true) {
+            for (Channel channel : channels) {
+                channel.writeAndFlush(TransferDataByTest() + "\n");
             }
+            Thread.sleep(1000);
         }
+    }
+
+    private String TransferDataByTest() {
+        Map<String, Object> maps = new HashMap<>();
+
+        maps.put("computerDate", "0天0时46分40秒");
+
+        // cpu负荷，展示每秒的使用占比：23%
+        Map<String, String> cpuMap = new HashMap<>();
+        cpuMap.put("time", LocalTime.now().toString());
+        cpuMap.put("value", "23");
+        maps.put("cpu", cpuMap);
+        // 硬盘信息：根目录和home目录的使用占比（各自均为100%） ：23% 、34%
+        Map<String, String> hardMap = new HashMap<>();
+        hardMap.put("root", "23");
+        hardMap.put("home", "34");
+        maps.put("disk", hardMap);
+        // 内存状态 ：总计、未使用、使用、缓存、其他
+        Map<String, String> memoryMap = new HashMap<>();
+        memoryMap.put("total", "62.49GB");
+        memoryMap.put("notUsed", "32.20GB");
+        memoryMap.put("use", "21.32GB");
+        memoryMap.put("cache", "8.96GB");
+        memoryMap.put("other", "0KB");
+        maps.put("memory", memoryMap);
+        Map<String, String> networkMap = new HashMap<>();
+        networkMap.put("date", LocalTime.now().toString());
+        networkMap.put("rxkb", "73.68rxkB/s");
+        networkMap.put("txkb", "73.68txkB/s");
+        maps.put("network", networkMap);
+        return JSONObject.toJSONString(maps);
+
     }
 
     @Override
@@ -62,22 +93,30 @@ public class ChatServerHandler extends SimpleChannelInboundHandler<String> {
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(ctx.channel().remoteAddress() + "下线 \n");
         Channel incoming = ctx.channel();
         for (Channel channel : channels) {
             if (channel != incoming) {
-                System.out.println(incoming.remoteAddress() + "上线 \n" );
+                System.out.println(incoming.remoteAddress() + "上线 \n");
             }
         }
     }
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(ctx.channel().remoteAddress() + "下线 \n");
         Channel incoming = ctx.channel();
         for (Channel channel : channels) {
             if (channel != incoming) {
                 channel.writeAndFlush(incoming.remoteAddress() + "下线 \n");
-                System.out.println(incoming.remoteAddress() + "下线 \n" );
+                System.out.println(incoming.remoteAddress() + "下线 \n");
             }
         }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+//        super.exceptionCaught(ctx, cause);
+        System.out.println(ctx.channel().remoteAddress() + "下线 \n");
     }
 }
